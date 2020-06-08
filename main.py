c2cdc823dc200eea4c0d8207f5ce97ac4b18c30f
Erick <38795106+Birunda3000@users.noreply.github.com>
2020-06-08 18:20:52 -0300

    2

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
 import mlflow
+import mlflow.sklearn
 
-print('inicio2')
-
-mlflow.start_run(experiment_id=0)
-
-mlflow.log_param('b', 99)
-mlflow.log_metric('a', 99)
-
-mlflow.end_run()
-
-
-
-print('fim')
+if __name__ == "__main__":
+    X = np.array([-2, -1, 0, 1, 2, 1]).reshape(-1, 1)
+    y = np.array([0, 0, 1, 1, 1, 0])
+    lr = LogisticRegression()
+    lr.fit(X, y)
+    score = lr.score(X, y)
+    print("Score: %s" % score)
+    mlflow.log_metric("score", score)
+    mlflow.sklearn.log_model(lr, "model")
+    print("Model saved in run444 {}".format(mlflow.active_run().info.run_uuid))
+    print('fim')
